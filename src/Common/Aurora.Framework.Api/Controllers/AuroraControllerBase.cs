d42ef58652326cd999130396dd3d1e7d954d1ecf
gerardogarnica <gerardo.garnica@gmail.com>
2022-08-11 16:26:59 -0500

    Aurora.Framework.Api: add common controller base.

---
+++ src/Common/Aurora.Framework.Api/Controllers/AuroraControllerBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Aurora.Framework.Api
+{
+    [ProducesErrorResponseType(typeof(ErrorDetailResponse))]
+    public abstract class AuroraControllerBase : ControllerBase
+    {
+        #region Private members
+
+        protected readonly ILogger<AuroraControllerBase> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        public AuroraControllerBase(ILogger<AuroraControllerBase> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        #endregion
+    }
+}
