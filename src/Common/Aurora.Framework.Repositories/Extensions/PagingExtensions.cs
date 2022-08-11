b611b4de078a963e9ccf989d8b169e6c9fc21398
gerardogarnica <gerardo.garnica@gmail.com>
2022-08-11 16:24:00 -0500

    Aurora.Framework.Repositories: add paging logic.

---
+++ src/Common/Aurora.Framework.Repositories/Extensions/PagingExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Aurora.Framework.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aurora.Framework.Repositories.Extensions
+{
+    public static class PagingExtensions
+    {
+        public static async Task<PagedCollection<T>> ToPagedCollectionAsync<T>(this IQueryable<T> query,
+                                                                               PagedViewRequest viewRequest) where T : class
+        {
+            var startingElement = 0;
+            if (viewRequest.PageIndex > 0) startingElement = viewRequest.PageIndex * viewRequest.PageSize;
+
+            var result = new PagedCollection<T>
+            {
+                Items = await query.Skip(startingElement).Take(viewRequest.PageSize).ToListAsync(),
+                TotalItems = await query.CountAsync(),
+                CurrentPage = viewRequest.PageIndex + 1
+            };
+
+            if (result.TotalItems > 0)
+            {
+                result.TotalPages = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(result.TotalItems) / viewRequest.PageSize));
+            }
+
+            return result;
+        }
+    }
+}
