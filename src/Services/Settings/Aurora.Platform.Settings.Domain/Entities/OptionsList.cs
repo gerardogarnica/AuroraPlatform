592c51ebb265c1aee9f6d2ac9fa7f18d02d3072f
gerardogarnica <gerardo.garnica@gmail.com>
2022-08-19 16:17:46 -0500

    Aurora.Platform.Settings.Domain: add options list entities.

---
+++ src/Services/Settings/Aurora.Platform.Settings.Domain/Entities/OptionsList.cs
@@ -0,0 +1,14 @@
+﻿using Aurora.Framework.Entities;
+
+namespace Aurora.Platform.Settings.Domain.Entities
+{
+    public class OptionsList : EntityBase
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsVisible { get; set; }
+        public bool IsEditable { get; set; }
+        public IList<OptionsListItem>? Items { get; set; }
+    }
+}
