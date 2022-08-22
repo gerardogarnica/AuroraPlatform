8ff982113ff3fd04cce37d58ceb6a1e863fffaff
gerardogarnica <gerardo.garnica@gmail.com>
2022-08-21 19:10:31 -0500

    Aurora.Platform.Settings.Domain: add options list repositories.

---
+++ src/Services/Settings/Aurora.Platform.Settings.Domain/Exceptions/OptionsListException.cs
@@ -0,0 +1,10 @@
+﻿using Aurora.Framework.Exceptions;
+
+namespace Aurora.Platform.Settings.Domain.Exceptions
+{
+    public class OptionsListException : BusinessException
+    {
+        public OptionsListException(string message)
+            : base("OptionsListException", message) { }
+    }
+}
---
+++ src/Services/Settings/Aurora.Platform.Settings.Domain/Repositories/IOptionsListRepository.cs
@@ -0,0 +1,10 @@
+﻿using Aurora.Framework.Repositories;
+using Aurora.Platform.Settings.Domain.Entities;
+
+namespace Aurora.Platform.Settings.Domain.Repositories
+{
+    public interface IOptionsListRepository : IReadableRepository<OptionsList>, IWriteableRepository<OptionsList>
+    {
+        Task<OptionsList?> GetByCodeAsync(string code);
+    }
+}
