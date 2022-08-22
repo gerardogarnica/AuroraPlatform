e3f4a8deb0bb66b6355e5b16051163cef5ecad90
gerardogarnica <gerardo.garnica@gmail.com>
2022-08-22 18:15:03 -0500

    Aurora.Platform.Settings.Infrastructure: add settings repository implementation.

---
+++ src/Services/Settings/Aurora.Platform.Settings.Infrastructure/Repositories/OptionsListRepository.cs
@@ -0,0 +1,35 @@
+﻿using Aurora.Framework.Repositories;
+using Aurora.Platform.Settings.Domain.Entities;
+using Aurora.Platform.Settings.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aurora.Platform.Settings.Infrastructure.Repositories
+{
+    public class OptionsListRepository : RepositoryBase<OptionsList>, IOptionsListRepository
+    {
+        #region Private members
+
+        private readonly SettingsContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        public OptionsListRepository(SettingsContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        async Task<OptionsList?> IOptionsListRepository.GetByCodeAsync(string code)
+        {
+            return await _context
+                .Options
+                .AsNoTracking()
+                .Include(x => x.Items)
+                .FirstOrDefaultAsync(x => x.Code == code);
+        }
+    }
+}
---
+++ src/Services/Settings/Aurora.Platform.Settings.Infrastructure/ServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Aurora.Platform.Settings.Domain.Repositories;
+using Aurora.Platform.Settings.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aurora.Platform.Settings.Infrastructure
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection AddSettingsRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Repositories implementations
+            services.AddScoped<IOptionsListRepository, OptionsListRepository>();
+
+            return services;
+        }
+    }
+}
