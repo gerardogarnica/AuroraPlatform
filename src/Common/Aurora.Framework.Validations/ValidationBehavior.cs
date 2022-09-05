180866d5c6b4e1a5618359cafcc7e13a0c9d3aa6
gerardogarnica <gerardo.garnica@gmail.com>
2022-09-05 10:50:37 -0500

    Aurora.Framework.Validations: add FluentValidations with MediatR.

---
+++ src/Common/Aurora.Framework.Validations/ValidationBehavior.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Aurora.Framework.Validations
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        #region Private members
+
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        #endregion
+
+        #region Constructors
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        #endregion
+
+        #region IPipelineBehavior interface implementation
+
+        async Task<TResponse> IPipelineBehavior<TRequest, TResponse>.Handle(
+            TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var failures = _validators
+                    .Select(v => v.Validate(request))
+                    .SelectMany(r => r.Errors)
+                    .Where(f => f != null)
+                    .Select(f => f.ErrorMessage)
+                    .ToList();
+
+                if (failures.Any())
+                {
+                    throw new Exceptions.ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Common/Aurora.Framework.Validations/ValidatorsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aurora.Framework.Validations
+{
+    public static class ValidatorsConfiguration
+    {
+        public static IServiceCollection AddValidationServices(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
