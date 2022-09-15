3011bb78188dcfbb90fafc686cb6ca8e0bafff32
gerardogarnica <gerardo.garnica@gmail.com>
2022-09-15 12:10:15 -0500

    Aurora.Platform.Settings.Application: add mapper profile.

---
+++ src/Services/Settings/Aurora.Platform.Settings.Application/Mappings/MapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using Aurora.Platform.Settings.Application.Commands;
+using Aurora.Platform.Settings.Application.Queries;
+using Aurora.Platform.Settings.Domain.Entities;
+using AutoMapper;
+using OptionsListItem = Aurora.Platform.Settings.Domain.Entities.OptionsListItem;
+using OptionsListItemViewModel = Aurora.Platform.Settings.Application.Queries.OptionsListItem;
+
+namespace Aurora.Platform.Settings.Application.Mappings
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            // Source: entity. Destination: view model.
+            CreateMap<OptionsList, OptionsListViewModel>();
+            CreateMap<OptionsListItem, OptionsListItemViewModel>();
+
+            // Source: command. Destination: entity.
+            CreateMap<CreateOptionsListCommand, OptionsList>()
+                .ForMember(d => d.Code, o => o.MapFrom(o => o.Code != null ? o.Code.Trim() : string.Empty))
+                .ForMember(d => d.Name, o => o.MapFrom(o => o.Name != null ? o.Name.Trim() : string.Empty))
+                .ForMember(d => d.Description, o => o.MapFrom(o => o.Description != null ? o.Description.Trim() : string.Empty));
+
+            CreateMap<CreateOptionsListItem, OptionsListItem>();
+        }
+    }
+}
