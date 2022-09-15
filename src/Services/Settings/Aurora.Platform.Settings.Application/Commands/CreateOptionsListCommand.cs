ed014c0d0c4b48cbd1b8589164e418104d5af7d6
gerardogarnica <gerardo.garnica@gmail.com>
2022-09-15 12:07:52 -0500

    Aurora.Platform.Settings.Application: add create options list command.

---
+++ src/Services/Settings/Aurora.Platform.Settings.Application/Commands/CreateOptionsListCommand.cs
@@ -0,0 +1,23 @@
+﻿using Aurora.Platform.Settings.Application.Queries;
+using MediatR;
+
+namespace Aurora.Platform.Settings.Application.Commands
+{
+    public class CreateOptionsListCommand : IRequest<OptionsListViewModel>
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsVisible { get; set; }
+        public bool IsEditable { get; set; }
+        public IList<CreateOptionsListItem>? Items { get; set; } = new List<CreateOptionsListItem>();
+    }
+
+    public class CreateOptionsListItem
+    {
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+        public bool IsEditable { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ src/Services/Settings/Aurora.Platform.Settings.Application/Handler/CreateOptionsListCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Aurora.Platform.Settings.Application.Commands;
+using Aurora.Platform.Settings.Application.Queries;
+using Aurora.Platform.Settings.Domain.Entities;
+using Aurora.Platform.Settings.Domain.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Aurora.Platform.Settings.Application.Handler
+{
+    public class CreateOptionsListCommandHandler : IRequestHandler<CreateOptionsListCommand, OptionsListViewModel>
+    {
+        private readonly IOptionsListRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CreateOptionsListCommandHandler(IOptionsListRepository repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        async Task<OptionsListViewModel> IRequestHandler<CreateOptionsListCommand, OptionsListViewModel>.Handle(
+            CreateOptionsListCommand request, CancellationToken cancellationToken)
+        {
+            var entry = _mapper.Map<OptionsList>(request);
+
+            var optionList = await _repository.AddAsync(entry);
+            return _mapper.Map<OptionsListViewModel>(optionList);
+        }
+    }
+}
