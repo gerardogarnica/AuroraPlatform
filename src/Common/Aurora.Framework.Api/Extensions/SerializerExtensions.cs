c1888bc531c575acd0e654cbc993d3a61bda5b71
gerardogarnica <gerardo.garnica@gmail.com>
2022-09-30 15:14:14 -0500

    Aurora.Framework.Api: add serializer extensions.

---
+++ src/Common/Aurora.Framework.Api/Extensions/SerializerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Serialization;
+
+namespace Aurora.Framework.Api
+{
+    public static class SerializerExtensions
+    {
+        public static IServiceCollection AddStringEnumConverter(this IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(a =>
+            {
+                a.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+            return services;
+        }
+    }
+}
