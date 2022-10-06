6553c36642ba6f05b08ac05125b4cbfddb52c826
gerardogarnica <gerardo.garnica@gmail.com>
2022-10-06 14:13:57 -0500

    Aurora.Platform.Settings.Domain: add new exception type to AttributeException.

--- src/Services/Settings/Aurora.Platform.Settings.Domain/Exceptions/AttributeException.cs
+++ src/Services/Settings/Aurora.Platform.Settings.Domain/Exceptions/AttributeException.cs
@@ -5,6 +5,7 @@ namespace Aurora.Platform.Settings.Domain.Exceptions
     public class AttributeException : BusinessException
     {
         protected const string AttributeNullMessage = "The record of the attribute cannot be null.";
+        protected const string InvalidSettingCodeMessage = "The code '{0}' does not exist.";
 
         public AttributeException(string message)
             : base("AttributeException", message) { }
@@ -14,5 +15,11 @@ namespace Aurora.Platform.Settings.Domain.Exceptions
     {
         public AttributeNullException()
             : base(AttributeNullMessage) { }
+    }
+
+    public class InvalidSettingCodeException : AttributeException
+    {
+        public InvalidSettingCodeException(string code)
+            : base(string.Format(InvalidSettingCodeMessage, code)) { }
     }
 }
