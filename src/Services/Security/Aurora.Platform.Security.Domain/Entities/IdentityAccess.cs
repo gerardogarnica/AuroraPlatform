1d1f3064da91344b41bf92de4fd8a6ea7aa4d5f5
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-11 15:25:23 -0500

    Add Aurora.Platform.Security.Domain library.

---
+++ src/Services/Security/Aurora.Platform.Security.Domain/Entities/IdentityAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace Aurora.Platform.Security.Domain.Entities
+{
+    public class IdentityAccess
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
