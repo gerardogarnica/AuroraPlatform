cc56e6c4914404ddebcefb18b778934c53a309e3
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-11 15:43:01 -0500

    Add Aurora.Platform.Security.Infrastructure library

---
+++ src/Services/Security/Aurora.Platform.Security.Infrastructure/InfrastructureServiceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aurora.Platform.Security.Infrastructure
+{
+    public static class InfrastructureServiceConfiguration
+    {
+        public static IServiceCollection AddSecurityInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Connection string
+            services.AddDbContext<SecurityContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("SecurityDataConnection")));
+
+            // Repository implementations
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Security/Aurora.Platform.Security.Infrastructure/SecurityContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aurora.Platform.Security.Infrastructure
+{
+    public class SecurityContext : DbContext
+    {
+        #region DbSet properties
+
+        #endregion
+
+        #region Constructors
+
+        public SecurityContext(DbContextOptions<SecurityContext> options)
+            : base(options) { }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
