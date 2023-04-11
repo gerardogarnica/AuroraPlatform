189537759c2108fa01b5c7cc5f6b65546f055332
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-11 15:26:18 -0500

    Add Aurora.Platform.Security.Application library

---
+++ src/Services/Security/Aurora.Platform.Security.Application/ApplicationServiceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Aurora.Framework.Validations;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Aurora.Platform.Security.Application
+{
+    public static class ApplicationServiceConfiguration
+    {
+        public static IServiceCollection AddSecurityApplicationServices(this IServiceCollection services)
+        {
+            // AutoMapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Validations
+            services.AddValidationServices();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // MediatR
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
