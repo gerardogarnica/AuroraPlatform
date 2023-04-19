d1125446a83030e898a5a27486cda657a00a7c39
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-19 15:35:42 -0500

    Aurora.Platform.Security.Application: add user login command.

---
+++ src/Services/Security/Aurora.Platform.Security.Application/UserLogin/UserLoginCommand.cs
@@ -0,0 +1,11 @@
+﻿using Aurora.Platform.Security.Domain.Entities;
+using MediatR;
+
+namespace Aurora.Platform.Security.Application.UserLogin
+{
+    public class UserLoginCommand : IRequest<IdentityAccess>
+    {
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/Services/Security/Aurora.Platform.Security.Application/UserLogin/UserLoginValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Aurora.Platform.Security.Application.UserLogin
+{
+    public class UserLoginValidator : AbstractValidator<UserLoginCommand>
+    {
+        public UserLoginValidator() 
+        {
+            RuleFor(p => p.LoginName)
+                .NotEmpty()
+                .WithMessage("Login name is required.");
+
+            RuleFor(p => p.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.");
+        }
+    }
+}
