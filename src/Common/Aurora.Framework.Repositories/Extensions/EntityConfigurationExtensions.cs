ec9611b50093721768cb7d1c5b86a2ed5a12848e
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-22 10:34:00 -0500

    Aurora.Framework.Repositories: add EntityConfigurationExtensions for auditable entities.

---
+++ src/Common/Aurora.Framework.Repositories/Extensions/EntityConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Aurora.Framework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aurora.Framework.Repositories
+{
+    public static class EntityConfigurationExtensions
+    {
+        public static void AddAuditableProperties<T>(this EntityTypeBuilder<T> builder) where T : AuditableEntity
+        {
+            builder.Property(e => e.CreatedBy).HasColumnName("CreatedBy").IsRequired().HasColumnType("varchar(35)");
+            builder.Property(e => e.CreatedDate).HasColumnName("CreatedDate").IsRequired().HasColumnType("datetime");
+            builder.Property(e => e.LastUpdatedBy).HasColumnName("LastUpdatedBy").IsRequired().HasColumnType("varchar(35)");
+            builder.Property(e => e.LastUpdatedDate).HasColumnName("LastUpdatedDate").IsRequired().HasColumnType("datetime");
+        }
+    }
+}
