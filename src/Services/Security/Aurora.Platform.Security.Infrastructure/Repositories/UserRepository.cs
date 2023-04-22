7d3fb8cff206d75e46013238cde11fe3bf0bd0ea
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-21 22:27:31 -0500

    Aurora.Platform.Security: add user repository.

---
+++ src/Services/Security/Aurora.Platform.Security.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using Aurora.Framework.Repositories;
+using Aurora.Platform.Security.Domain.Entities;
+using Aurora.Platform.Security.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aurora.Platform.Security.Infrastructure.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        #region Private members
+
+        private readonly SecurityContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        public UserRepository(SecurityContext context)
+            : base(context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        #endregion
+
+        #region IUserRepository implementation
+
+        async Task<User> IUserRepository.GetAsync(string loginName)
+        {
+            return await _context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Credential)
+                .Include(x => x.Token)
+                .Include(x => x.UserRoles)
+                .FirstOrDefaultAsync(x => x.LoginName.Equals(loginName));
+        }
+
+        #endregion
+    }
+}
