75514fd850f194030afb8d6ea21793b7ff69966f
gerardogarnica <gerardo.garnica@gmail.com>
2023-04-25 22:35:38 -0500

    Add Aurora.Platform.Security.API project.

---
+++ src/Services/Security/Aurora.Platform.Security.API/Program.cs
@@ -0,0 +1,41 @@
+using Aurora.Framework.Api;
+using Aurora.Framework.Logging;
+using Aurora.Framework.Security;
+using Aurora.Platform.Security.Application;
+using Aurora.Platform.Security.Infrastructure;
+
+const string apiName = "Aurora Platform Security";
+const string apiDescription = "Aurora Platform security services API.";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add API configuration.
+builder.Services.AddSecurityApplicationServices();
+builder.Services.AddSecurityInfrastructureServices(builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGenWithApiKey(apiName, apiDescription, 1);
+builder.Services.AddStringEnumConverter();
+builder.Services.AddAuthenticationServices(builder.Configuration);
+
+// Add Serilog configuration.
+builder.Host.ConfigureSerilog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware(typeof(ApiHandlerMiddleware));
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
