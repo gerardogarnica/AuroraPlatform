a347d8f252123e9884b0db4c5cbac0f8ca785a3c
gerardogarnica <gerardo.garnica@gmail.com>
2023-05-03 09:01:22 -0500

    Aurora.Framework.Common: add ApiAuthorizationException exception class.

---
+++ src/Common/Aurora.Framework.Common/Exceptions/ApiAuthorizationException.cs
@@ -0,0 +1,8 @@
+﻿namespace Aurora.Framework
+{
+    public class ApiAuthorizationException : Exception
+    {
+        public ApiAuthorizationException()
+            : base("There is no authorization for this operation.") { }
+    }
+}
