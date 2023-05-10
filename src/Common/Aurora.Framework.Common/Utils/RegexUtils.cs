8a1ba13828c8b1e98ca2d46e7d1a7d389e6fb5bc
gerardogarnica <gerardo.garnica@gmail.com>
2023-05-10 13:49:30 -0500

    Aurora.Framework: add RegexUtils class.

---
+++ src/Common/Aurora.Framework.Common/Utils/RegexUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Aurora.Framework.Utils
+{
+    public static partial class RegexUtils
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return EmailRegex().IsMatch(email);
+        }
+
+        [GeneratedRegex("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$")]
+        private static partial Regex EmailRegex();
+    }
+}
