b6e066e5997d72178d5f4628d24442fa1f4e0605
gerardogarnica <gerardo.garnica@gmail.com>
2023-10-05 15:51:41 -0500

    Aurora.Framework.Repositories: add validations to paging extensions.

--- src/Common/Aurora.Framework.Repositories/Extensions/PagingExtensions.cs
+++ src/Common/Aurora.Framework.Repositories/Extensions/PagingExtensions.cs
@@ -5,8 +5,8 @@ namespace Aurora.Framework.Repositories.Extensions
 {
     public static class PagingExtensions
     {
-        public static async Task<PagedCollection<T>> ToPagedCollectionAsync<T>(this IQueryable<T> query,
-                                                                               PagedViewRequest viewRequest) where T : class
+        public static async Task<PagedCollection<T>> ToPagedCollectionAsync<T>(
+            this IQueryable<T> query, PagedViewRequest viewRequest) where T : class
         {
             var startingElement = 0;
             if (viewRequest.PageIndex > 0) startingElement = viewRequest.PageIndex * viewRequest.PageSize;
@@ -17,13 +17,13 @@ namespace Aurora.Framework.Repositories.Extensions
                 TotalItems = await query.CountAsync(),
                 CurrentPage = viewRequest.PageIndex + 1
             };
+            result.TotalPages = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(result.TotalItems) / viewRequest.PageSize));
 
-            if (result.TotalItems > 0)
-            {
-                result.TotalPages = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(result.TotalItems) / viewRequest.PageSize));
-            }
+            if (result.TotalItems == 0) result.CurrentPage = 0;
 
-            return result;
+            return result.TotalPages < result.CurrentPage
+                ? throw new PlatformException("The current page cannot be greater than the total pages.")
+                : result;
         }
     }
 }
