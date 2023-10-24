aea3bde2254157a0c4c240970061b1cfddc2354c
gerardogarnica <gerardo.garnica@gmail.com>
2023-10-24 12:08:40 -0500

    Aurora.Framework.Repositories: add new sql types.

--- src/Common/Aurora.Framework.Repositories/SqlDataType.cs
+++ src/Common/Aurora.Framework.Repositories/SqlDataType.cs
@@ -5,15 +5,20 @@
         public const string Boolean = "bit";
         public const string DateTime = "datetime";
         public const string Description = "varchar(100)";
+        public const string Code = "varchar(40)";
+        public const string DataType = "varchar(15)";
         public const string Email = "varchar(50)";
         public const string Guid = "uniqueidentifier";
         public const string Int32 = "int";
+        public const string Name = "varchar(50)";
         public const string Notes = "nvarchar(2000)";
         public const string ProperName = "nvarchar(40)";
+        public const string VarChar25 = "varchar(25)";
         public const string VarChar50 = "varchar(50)";
         public const string VarChar100 = "varchar(100)";
         public const string VarChar200 = "varchar(200)";
         public const string VarChar1000 = "varchar(1000)";
         public const string VarChar4000 = "varchar(4000)";
+        public const string Xml = "xml";
     }
 }
