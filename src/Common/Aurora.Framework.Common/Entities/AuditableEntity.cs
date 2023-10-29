a853c7f20d1ce557786eb615d5c33303ff4d3660
gerardogarnica <gerardo.garnica@gmail.com>
2023-10-29 11:53:05 -0500

    Aurora.Framework.Common: moved entity classes.

---
+++ src/Common/Aurora.Framework.Common/Entities/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Aurora.Framework.Entities
+{
+    public abstract class AuditableEntity : EntityBase
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedDate { get; set; }
+    }
+}
---
+++ src/Common/Aurora.Framework.Common/Entities/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Aurora.Framework.Entities
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+    }
+}
